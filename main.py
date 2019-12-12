b8760f4b826f4f07bc7ba4f12b3b94495f1de43e
Shruthi K N <shruthikngowda@gmail.com>
2019-12-12 11:56:23 +0000

    10th attempt correct answer was giving unexpected answers. Resolved the issue.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ for i in secret_word:
     to_show_user.append("-")
 
 while no_of_guesses > 0:
-  if '-' in to_show_user:
     user_guess = input("Please enter a word ").lower()
     no_of_guesses = no_of_guesses - 1
     if len(user_guess) > 1:
@@ -26,10 +25,19 @@ while no_of_guesses > 0:
         if secret_word[x] == user_guess:
           to_show_user[x] = user_guess
       print("Guessed correctly")
+      if not '-' in to_show_user:
+        no_of_guesses = -1
+        print("Hurray! You won !")
+        sys.exit()
+
     else:
       print("Wrong guess !")
-    print("the secret word now is ",to_show_user)
+    #print(no_of_guesses)
+    if no_of_guesses != 0:
+      print("the secret word now is ",to_show_user)
   else:
     no_of_guesses = -1
     print("Hurray! You won !")
     sys.exit()
+
+print("Sorry, you lost the game ")
